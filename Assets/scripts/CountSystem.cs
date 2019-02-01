e5a27e7a1efda5cd80843790281ef4b3601c1fb0
delevinying <delevinyyc@gmail.com>
2019-02-01 09:25:34 +0800

    。
    。

---
+++ Assets/scripts/CountSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+public class CountSystem : ComponentSystem
+{
+    struct Group{
+        public int length;
+        public ComponentDataArray<CountData> countData;
+    }
+
+    [Inject] Group group;
+    protected override void OnUpdate(){
+        for (int i = 0; i < group.length; i++)
+        {
+            var countData = group.countData[i];
+            countData.count++;
+            group.countData[i] = countData;
+        }
+    }
+}
+
+//class CountData : IComponentData
+//{
+//    public int count;
+//}
---
+++ Assets/scripts/ECSMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+public class ECSMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        var sample = entityManager.CreateArchetype(typeof(CountData));
+
+        entityManager.CreateEntity(sample);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
